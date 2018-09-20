bf7edbb88daa4ee4861ecadced4ccfb0e89828cb
Tien Mai <maitien2004@xuongrong.info>
2018-09-20 21:13:04 +0700

    - Implement delete pool, wallet

--- server/controllers/ledger.ts
+++ server/controllers/ledger.ts
@@ -1,6 +1,26 @@
 import Ledger from '../models/ledger';
 import BaseCtrl from './base';
+import indy from 'indy-sdk';
 
 export default class LedgerCtrl extends BaseCtrl {
   model = Ledger;
+
+  delete = (req, res) => {
+    this.model.findOne({ _id: req.params.id }, (err, item) => {
+      try {
+        if (item.stewardName) {
+          let walletConfig = { 'id': item.stewardName + 'Wallet' };
+          let walletCredentials = { 'key': item.stewardName + '_key' };
+          indy.deleteWallet(walletConfig, walletCredentials);
+        }
+        if (item.poolName) indy.deletePoolLedgerConfig(item.poolName);
+        res.status(200).json(item);
+      } catch (e) {
+        console.log(e);
+        res.status(404).json({
+          message: 'Ledger not found.'
+        });
+      }
+    });
+  }
 }
--- server/controllers/trustAnchor.ts
+++ server/controllers/trustAnchor.ts
@@ -1,6 +1,25 @@
 import TrustAnchor from '../models/trustAnchor';
 import BaseCtrl from './base';
+import indy = require('indy-sdk');
 
 export default class TrustAnchorCtrl extends BaseCtrl {
   model = TrustAnchor;
+
+  delete = (req, res) => {
+    this.model.findOne({ _id: req.params.id }, (err, item) => {
+      try {
+        if (item.trustAnchorName) {
+          let walletConfig = { 'id': item.trustAnchorName + 'Wallet' };
+          let walletCredentials = { 'key': item.trustAnchorName + '_key' };
+          indy.deleteWallet(walletConfig, walletCredentials);
+        }
+        res.status(200).json(item);
+      } catch (e) {
+        console.log(e);
+        res.status(404).json({
+          message: 'Trust Anchor not found.'
+        });
+      }
+    });
+  }
 }
